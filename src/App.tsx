481113cabd4ec419ea38da0c9a1c204565217761
Murilo Missura <murilo@Mac-mini-de-Murilo.local>
2024-11-18 17:42:15 -0300

    refactor: improvementes main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,54 @@
+import { useState } from 'react';
 import { BsWhatsapp } from 'react-icons/bs';
-import Logo from './assets/images/Logo-USA.png'
-import Img from './assets/images/img2.jpg'
-import Img3 from './assets/images/img3.jpg'
-import Img4 from './assets/images/img4.jpg'
-import Img5 from './assets/images/img5.jpg'
+import Logo from './assets/images/logo-usa1.svg';
+import Img from './assets/images/img1.png';
+import Img3 from './assets/images/img2.png';
+import Img4 from './assets/images/img3.png';
+import Img5 from './assets/images/img4.png';
+import Spain from './assets/images/spain.png';
+import Usa from './assets/images/usa.png';
+
+import { LANGUAGES } from './intl';
 
 export const App = () => {
-
+  type languageProps = 'US' | 'ES';
+  const [language, setLanguage] = useState<languageProps>('US');
 
   return (
-    <div className='w-full'>
-      <div className='w-3/4 m-auto grid grid-cols-2 justify-beetween items-center'>
-        <div className='w-full'>
+    <div className="w-full">
+      <div className="flex justify-center items-center ">
+        <div className="w-full">
           <img
             src={Logo}
-            className='w-full justify-center'
-            alt='Logo PD Energy'
-            title='Logo PD Energy'
+            className="2xl:w-1/2 xl:w-1/2 lg:w-1/2 w-3/4 2xl:mx-24 xl:mx-24 lg:mx-24 mx-6"
+            alt="Logo PD Energy"
+            title="Logo PD Energy"
           />
         </div>
 
-        <nav className='w-full'>
-          <ul className='flex justify-end gap-4'>
+        <nav className="w-full">
+          <div className="flex flex-col-2 justify-end itemns-center mx-auto px-4 mt-4">
+            <button
+              onClick={() => setLanguage('ES')}
+              className="border-sm rounded-md p-2 mb-2 w-12"
+            >
+              <img src={Spain} alt="Translate to Spanish" />
+            </button>
+            <button
+              onClick={() => setLanguage('US')}
+              className="border-sm rounded-md p-2 mb-2 w-12"
+            >
+              <img src={Usa} alt="Translate page to English" />
+            </button>
+          </div>
 
+          <ul className="flex justify-end 2xl:px-24 xl:px-24 lg:px-24 px-4">
             <a
-              href='https://wa.me/14078677407'
-              target='_blank'
-              className='bg-[#498b00] text-white 2xl:text-xl xl:text-xl lg:text-xl text-xl 
+              href="https://api.whatsapp.com/send/?phone=5519991711316&text=Ol%C3%A1%21+Gostaria+de+mais+informa%C3%A7%C3%B5es+sobre+gera%C3%A7%C3%A3o+de+energia+solar+rural."
+              target="_blank"
+              className="bg-[#498b00] text-white 2xl:text-xl xl:text-xl lg:text-xl text-xl 
               py-3 px-4 2xl:px-4 xl:px-4 lg:px-4 flex justify-center items-center 
-              gap-3 rounded-xl my-12 cursor-pointer hover:bg-green-900'
+              gap-3 rounded-xl my-12 cursor-pointer hover:bg-green-900"
             >
               <BsWhatsapp /> WhatsApp
             </a>
@@ -36,143 +56,126 @@ export const App = () => {
         </nav>
       </div>
 
-      <div className='bg-image'>
-        <div className='w-full bg-black bg-opacity-40 py-14'>
-          <div
-            className='2xl:w-3/4 xl:w-3/4 lg:w-3/4 w-full h-full m-auto 
-          grid 2xl:grid-cols-2 xl:grid-cols-2 grid-cols-1 p-4 gap-6 justify-between items-center'
-          >
-            <div className='w-full'>
-            </div>
-            <div className='mb-10 text-white 2xl:text-3xl lg:text-3xl text-3xl text-center font-bold'>
-              <p>PD Energy USA designs, installs, and maintains solar panels for homes.<br></br>
+      <div className="bg-image">
+        <div className="w-full px-8 py-24 bg-black bg-opacity-50 flex justify-end items-center">
+          <div className="2xl:w-1/2 xl:w-1/2 lg:w-1/2 w-full">
+            <p className="text-white 2xl:text-3xl lg:text-3xl text-3xl text-center font-bold">
+              {LANGUAGES[language].HERO_TEXT.P1} <br></br>
+              {LANGUAGES[language].HERO_TEXT.P2} <br></br>
+              <br></br>
+              {LANGUAGES[language].HERO_TEXT.P3}
+            </p>
 
-                With solar energy, you can save on your energy bill, increase the value of your home, and become sustainable.<br></br><br></br>
-
-                Contact us to learn more.
-
-                <div className='flex justify-center items-center'>
-                  <div className='w-full flex justify-center items-center'>
-
-                    <a
-                      href='https://wa.me/14078677407'
-                      target='_blank'
-                      className='bg-[#498b00] text-white 2xl:text-xl xl:text-xl lg:text-xl text-lg py-3 px-4 
-                        2xl:px-4 xl:px-4 lg:px-4 flex justify-center items-center gap-3 rounded-xl my-12 cursor-pointer hover:bg-green-900'
-                    >
-                      <BsWhatsapp /> Contact Us
-                    </a>
-                  </div>
-                </div>
-              </p>
+            <div className="w-full flex justify-center items-center mt-8">
+              <a
+                href="https://api.whatsapp.com/send/?phone=5519991711316&text=Ol%C3%A1%21+Gostaria+de+mais+informa%C3%A7%C3%B5es+sobre+gera%C3%A7%C3%A3o+de+energia+solar+rural."
+                target="_blank"
+                className="bg-[#498b00] text-white 2xl:text-xl xl:text-xl lg:text-xl text-lg py-3 px-4 
+                        2xl:px-4 xl:px-4 lg:px-4 flex justify-center items-center gap-3 rounded-xl cursor-pointer hover:bg-green-900"
+              >
+                <BsWhatsapp /> {LANGUAGES[language].WHATSAPP_TEXT}
+              </a>
             </div>
           </div>
         </div>
       </div>
 
-      <section
-        className='2xl:w-1/2 xl:w-1/2 w-full h-full m-auto grid 2xl:grid-cols-0 mt-20
-      xl:grid-cols-0 grid-cols-2 p-4 gap-6 justify-center items-center'
-      >
-        <div className='grid grid-cols-2 gap-12'>
-          <img
-            src={Img}
-            className='w-full justify-center border-lg rounded-md'
-            alt='Home Solar Systems'
-            title='Home Solar Systems'
-          />
-          <div>
-            <h2 className='text-center text-lg font-bold'>Home Solar Systems</h2>
-            <p>Achieve energy independence, reduce your electricity bill with PD Energy USA's solar system.</p>
+      <section className="w-full mt-20 mx-auto p-4 grid 2xl:grid-cols-2 xl:grid-cols-2 lg:grid-cols-2 grid-cols-1 justify-center items-center gap-6">
+        <div>
+          <h2 className="text-center text-2xl text-green-600 font-bold">
+            {LANGUAGES[language].SERVICES_TEXT1.TITLE}
+          </h2>
+          <p className="text-slate-600 my-3 h-14 flex justify-center items-center">
+            {LANGUAGES[language].SERVICES_TEXT1.P1}
+          </p>
+          <div className="w-full overflow-hidden">
+            <img
+              src={Img}
+              className="w-full justify-center border-2 border-slate-200 shadow-md rounded-md"
+              alt="Home Solar Systems"
+              title="Home Solar Systems"
+            />
           </div>
         </div>
 
-        <section
-          className='2xl:w-1/2 xl:w-1/2 w-full h-full m-auto grid 2xl:grid-cols-0 mt-20
-      xl:grid-cols-0 grid-cols-2 p-4 gap-6 justify-center items-center'
-        >
-          <div className='grid grid-cols-2 gap-12'>
+        <div>
+          <h2 className="text-center text-2xl text-green-600 font-bold">
+            Panel Cleaning
+          </h2>
+          <p className="text-slate-600 my-3 h-14 flex justify-center items-center">
+            Cleaning the panel is essential for maximum performance of your
+            system.
+          </p>
+          <div className="w-full overflow-hidden">
             <img
               src={Img3}
-              className='w-full justify-center border-lg rounded-md'
-              alt='Panel cleaning'
-              title='Panel cleaning'
+              className="w-full justify-center border-lg rounded-md"
+              alt="Panel cleaning"
+              title="Panel cleaning"
             />
-            <div>
-              <h2 className='text-center text-lg font-bold'>Panel Cleaning</h2>
-              <p>Panel cleaning is essential for greater energy generation.</p>
-            </div>
           </div>
-        </section>
-      </section>
+        </div>
 
-      <section
-        className='2xl:w-1/2 xl:w-1/2 w-full h-full m-auto grid 2xl:grid-cols-0 mt-20
-      xl:grid-cols-0 grid-cols-2 p-4 gap-6 justify-center items-center'
-      >
         <div>
-          <img
-            src={Img4}
-            className='w-full justify-center'
-            alt='Solar Battery'
-            title='Solar Battery'
-          />
-          <div>
-            <h2 className='text-center text-lg font-bold'>Solar Battery</h2>
-            <p>You can store the energy generated by the panels using batteries, and use the energy when necessary. In the event of a power outage, you will continue to use energy in your home.</p>
+          <h2 className="text-center text-2xl text-green-600 font-bold">
+            Solar Battery
+          </h2>
+          <p className="text-slate-600 my-3 h-14 flex justify-center items-center">
+            You can store the energy generated by the panels using batteries,
+            and use the energy when necessary.
+          </p>
+          <div className="w-full overflow-hidden">
+            <img
+              src={Img4}
+              className="w-full justify-center"
+              alt="Solar Battery"
+              title="Solar Battery"
+            />
+          </div>
+        </div>
+
+        <div>
+          <h2 className="text-center text-2xl text-green-600 font-bold">
+            Electric Car Charger
+          </h2>
+          <p className="text-slate-600 my-3 h-14 flex justify-center items-center">
+            PD Energy USA installs and services electric vehicle chargers.
+          </p>
+          <div className="w-full overflow-hidden">
+            <img
+              src={Img5}
+              className="w-full justify-center"
+              alt="Electric Car Charger"
+              title="Electric Car Charger"
+            />
           </div>
         </div>
       </section>
 
-      <section
-        className='2xl:w-1/2 xl:w-1/2 w-full h-full m-auto grid 2xl:grid-cols-0 mt-20
-      xl:grid-cols-0 grid-cols-2 p-4 gap-6 justify-center items-center'
-      >
-        <div>
-          <img
-            src={Img5}
-            className='w-full justify-center'
-            alt='Electric Car Charger'
-            title='Electric Car Charger'
-          />
-          <div>
-            <h2 className='text-center text-lg font-bold'>Electric Car Charger</h2>
-            <p>PD Energy USA Installs Electric Vehicle Chargers.</p>
-          </div>
-        </div>
-      </section>
-
-
-
-      <h1
-        className='text-xl text-center 2xl:text-3xl xl:text-3xl lg:text-3xl 
-           my-20 mx-auto 2xl:w-2/3 xl:w-2/3 lg:w-2/3 w-full font-bold px-4'
-      >
-        Want to know more? Request a quote now.
-      </h1>
-
-      <section className='flex justify-center items-center mb-10'>
+      <section className="flex justify-center items-center mt-12">
         <a
-          href='https://wa.me/14078677407'
-          target='_blank'
-          className='bg-[#498b00] text-white py-3 px-10 flex justify-center items-center gap-3 rounded-xl mb-12 cursor-pointer hover:bg-green-900'
+          href="https://api.whatsapp.com/send/?phone=5519991711316&text=Ol%C3%A1%21+Gostaria+de+mais+informa%C3%A7%C3%B5es+sobre+gera%C3%A7%C3%A3o+de+energia+solar+rural."
+          target="_blank"
+          className="bg-[#498b00] text-white py-3 px-10 flex justify-center items-center gap-3 rounded-xl mb-20 cursor-pointer hover:bg-green-900"
         >
           <BsWhatsapp /> Contact Us
         </a>
       </section>
 
-
-
-
-
       <footer
-        className='text-white bg-gradient-to-r 
+        className="text-white bg-gradient-to-r 
         opacity-80 from-cyan-500 to-blue-500 py-12 px-20 
-        shadow-lg w-full text-center text-sm m-auto'
+        shadow-lg w-full text-center text-sm m-auto"
       >
         Copyright © 2024 PD Energy USA. All rights reserved. Developed by
-        <a href="http://missura.com.br"
-          target='_blank'> Missura Networks.</a>
+        <a
+          href="http://missura.com.br"
+          target="_blank"
+          className="text-white hover:text-black"
+        >
+          {' '}
+          Missura Networks.
+        </a>
       </footer>
     </div>
   );
---
+++ src/intl/index.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const LANGUAGES: any = {
+  US: {
+    WHATSAPP_TEXT: 'Contact Us',
+    HERO_TEXT: {
+      P1: 'PD Energy USA designs, installs, and maintains solar panels for homes.',
+      P2: 'With solar energy, you can save on your energy bill, increase the value of your home, and become sustainable.',
+      P3: 'Contact us to learn more.',
+    },
+    SERVICES_TEXT1: {
+      TITLE: 'Home Solar Systems',
+      P1: 'Achieve energy independence, reduce your electricity bill with PD Energy USA solar system.',
+    },
+  },
+  ES: {
+    WHATSAPP_TEXT: 'Contáctenos',
+    HERO_TEXT: {
+      P1: 'PD Energy USA diseña, instala y mantiene paneles solares para hogares.',
+      P2: 'Con la energía solar, puedes ahorrar en tu factura de electricidad, aumentar el valor de tu hogar y ser sostenible.',
+      P3: 'Contáctanos para obtener más información.',
+    },
+    SERVICES_TEXT1: {
+      TITLE: 'Sistemas Solares Residenciales',
+      P1: 'Logra independencia energética, reduce tu factura de electricidad con el sistema solar de PD Energy USA.',
+    },
+  },
+};
